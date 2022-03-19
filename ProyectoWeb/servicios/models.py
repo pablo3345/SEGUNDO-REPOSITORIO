0879d4189f862073ad4954e340bb1d7ee5037ff1
pablo3345 <pabloandresperuchi@gmail.com>
2022-03-19 17:56:06 -0300

    seguir practicando

--- ProyectoWeb/servicios/models.py
+++ ProyectoWeb/servicios/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class servicio(models.Model):
     titulo = models.CharField(max_length=50)
     contenido = models.CharField(max_length=50)
-    imagen = models.ImageField(upload_to='servicios')# con esto dentro de la carpeta media me va a crear una subcarpeta 'servicios' donde va a incluir las imagenes
+    imagen = models.ImageField(upload_to='servicios')#con esto dentro de la carpeta media me va a crear una subcarpeta 'servicios' donde va a incluir las imagenes
     created = models.DateTimeField(auto_now_add=True) #aca guardamos la fecha que se creo un servicio
     updated = models.DateTimeField(auto_now_add=True) # si este servicio se ha modificado, guardamos la fecha que se modifico
 
