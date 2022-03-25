0847ea905d293e1027d4444901686eb1b965aaeb
pablo3345 <pabloandresperuchi@gmail.com>
2022-03-25 16:46:33 -0300

    terminamo el login

--- ProyectoWeb/autenticacion/urls.py
+++ ProyectoWeb/autenticacion/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 
 
-from .views import VRegistro, cerrar_sesion
+from .views import VRegistro, cerrar_sesion, logear
 
 
 urlpatterns = [
@@ -11,6 +11,7 @@ urlpatterns = [
 
     path('', VRegistro.as_view(), name="Autenticacion"), # es_view es para que nos muestre esta clase como una vista
     path('cerrar_sesion', cerrar_sesion, name="Cerrar_sesion"),
+    path('logear', logear, name="Logear"),
 
 
 
--- ProyectoWeb/autenticacion/views.py
+++ ProyectoWeb/autenticacion/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 # Create your views here.
 
@@ -32,3 +32,26 @@ class VRegistro(View): # una clase que se transforma en vista
 def cerrar_sesion(request): #una nueva vista
     logout(request)
     return redirect('Home')
+
+
+def logear(request):
+
+    if request.method == "POST": # si ha apretado el boton, si hizo un post
+        form = AuthenticationForm(request, data=request.POST) # aca en form me guarda los datos del formulario con los datos
+        if form.is_valid():
+            nombre_usuario = form.cleaned_data.get("username") #dame la informacion que tengo guardado en el cuadro de texto que por defecto se llama "usurname"
+            contraseniaa = form.cleaned_data.get("password")
+            usuario = authenticate(username= nombre_usuario, password = contraseniaa) # la forma de autenticar el usuario
+            if usuario is not None: # si hay algo como unusuario y contraseña,  entonces me haces un login
+                login(request, usuario)
+                return redirect('Home')
+            else:
+                messages.error(request, "usuario no valido") # si no hay usuario entonce me pones este mensaje
+        else:
+            messages.error(request, "informacion incorrecta")  # este else viene de arriba, si el usuario no ha ingresado los datos correctamente
+
+
+
+
+    form = AuthenticationForm() # con esto ya tenemos en form el formulario de login
+    return render(request, "login/login.html", {"form": form}) # esta dos lineas de codigo es x si el usuario no le da al boton de logear, si no hace un post
