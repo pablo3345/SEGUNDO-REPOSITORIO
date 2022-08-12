82e4e10346a12abe36c5d1cc108df8e4bf26f276
pablo3345 <pabloandresperuchi@gmail.com>
2022-08-12 15:33:41 -0300

    ProyectoWeb finalizado por completo

--- ProyectoWeb/pedidoss/views.py
+++ ProyectoWeb/pedidoss/views.py
@@ -1,11 +1,14 @@
 from django.contrib.auth.decorators import login_required
+from django.core.mail import send_mail
 from django.shortcuts import render, redirect
 
 # Create your views here.
+from django.utils.html import strip_tags
 
 from carro.carro import Carro
 from pedidoss.models import Pedido, LineaPedido
 from django.contrib import messages
+from django.template.loader import render_to_string
 
 #@login_required(login_url="autenticacion/logear") #especificamos la url de logear por si el usuario no esta logeado y quiera acceder entonces este decorador lo lleva a la url de logear, esto es si el carro aparece siempre visible
 def procesar_pedido(request):
@@ -18,7 +21,7 @@ def procesar_pedido(request):
     for key, value in carro.carro.items(): # para recorrer el carro x cada clave valor de los items del carro lo agregamos a la lista lineas_pedidos y por eso llamo al modelo LineaPedido
         lineas_pedidos.append(LineaPedido(
 
-            producto_id = key, #la clave key seria igual al id del pedido
+            producto_id = key, #la clave key seria igual al id del pedido,,,,producto_id fijarse en el modelo xq en LineaPedido juan lo corrigio a pedido (pero parece que funciona lo mismo)
             cantidad = value['cantidad'],
             user= request.user, #necesito meter tambien el usuario
             pedido=pedido # tambien metemos el pedido
@@ -28,13 +31,13 @@ def procesar_pedido(request):
 
     LineaPedido.objects.bulk_create(lineas_pedidos) # con esto guardamos la lista en la BBDD, LineaPedido que es el modelo, con bulk_create seria como muchos insert into en la BBDD
 
-        #enviar_mail(pedido=pedido,
-         #           lineas_pedidos=lineas_pedidos,
-          #          nombreusuario = request.username,
-           #         emailusuario = request.usermail
+    enviar_mail(pedido=pedido,
+                lineas_pedidos=lineas_pedidos,
+                nombreusuario = request.user.username,
+                emailusuario = request.user.email
 
 
-                 #   )
+                    )
 
 
     messages.success(request, "El pedido se ha realizado correctamente")
@@ -44,7 +47,32 @@ def procesar_pedido(request):
 
 
 
-#def enviar_mail(**kwargs):
+def enviar_mail(**kwargs):
 
 
- #  asunto = "Gracias por el pedido"
+   asunto = "Gracias por el pedido"
+   mensaje= render_to_string("emails/pedido.html", {
+
+       "pedido": kwargs.get("pedido"),
+       "lineas_pedidos": kwargs.get("lineas_pedidos"),
+       "nombreusuario": kwargs.get("nombreusuario")
+
+
+
+
+
+   }) # aca vamos a guardar todas las lineas de pedido como lineas pedidos de arriba, con render_to_string le decimos que renderice el siguiente html
+
+   mensaje_texto=strip_tags(mensaje) # esto va a ser igual a la variable mensaje que creamos arriba donde hemos renderizado toda la informacion, pero ignorando toda etiqueta html como esta<> lo hacemos con strip_tags, le decimos que ignore las etiquetas html de la variable mensaje
+
+   from_mail="pabloandresperuchi@gmail.com" # de quien es el correo electronico, el de la tienda
+   #to=  kwargs.get("emailusuario")# el destinatario de los correos, con kwargs.get() obtengo los parametros de la funcion de arriba que le pase enviar_mail(), como no puse ningun usuario con un correo valido a esto lo comento y pongo mi correo personal
+   to= "sumerx21@yahoo.com.ar"
+
+
+
+   #ahora entra la funcion send_mail para enviar el mail
+
+   send_mail(asunto, mensaje_texto, from_mail, [to], html_message=mensaje) #mensaje texto es el mensaje sin etiquetas html, cuando enviamos un mensaje html debemos especificarlo con html_message=
+
+
